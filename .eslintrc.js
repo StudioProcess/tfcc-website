79ef83ebb93c5468bc551c9bcc35e8d73f7151e3
Martin Grödl <martin@process.studio>
2021-05-25 15:13:46 +0100

    add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  extends: [
+    '@nuxtjs',
+    'plugin:nuxt/recommended'
+  ],
+  plugins: [
+  ],
+  ignorePatterns: ['**/assets/*.js'],
+  // add your custom rules here
+  rules: {
+    'vue/singleline-html-element-content-newline': 'off',
+    'vue/multiline-html-element-content-newline': 'off',
+    'vue/max-attributes-per-line': 'off',
+    'vue/attribute-hyphenation': 'off'
+  }
+}
